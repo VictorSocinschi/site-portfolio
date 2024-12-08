0c5846ccd0bbb1d641dcf19d0f9a0ac78e572e84
Victor Socinschi <chaosdaddy84@gmail.com>
2024-12-08 15:14:19 +0200

    Added our client section html ,css and js

--- js/main.js
+++ js/main.js
@@ -44,3 +44,19 @@ portfolioCloseBtn.forEach((button) => {
     });
   });
 });
+
+// Swiper initialization
+
+var swiper = new Swiper(".client-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
