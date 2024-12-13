7375c9abeb7df7d13454559eab7315f082682b5c
Victor Socinschi <chaosdaddy84@gmail.com>
2024-12-13 18:06:53 +0200

    Added media querry  and scroll reveal

--- js/main.js
+++ js/main.js
@@ -1,5 +1,65 @@
-//Services section -Modal
+//Header effect scroll
 
+const header = document.querySelector(".header");
+window.addEventListener("scroll", function () {
+  window.scrollY > 1
+    ? header.classList.add("sticky")
+    : header.classList.remove("sticky");
+});
+
+//Nagigation meny active
+window.addEventListener("scroll", function () {
+  const section = document.querySelectorAll("section");
+  const scrollY = this.window.scrollY;
+
+  section.forEach(function (current) {
+    let sectionHeight = current.offsetHeight;
+    let sectionTop = current.offsetTop - 50;
+    let sectionId = current.getAttribute("id");
+    let navItem = document.querySelector(`.nav-item a[href*="${sectionId}"]`);
+
+    if (navItem) {
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+        navItem.classList.add("active");
+      } else {
+        navItem.classList.remove("active");
+      }
+    }
+  });
+});
+
+//scrollToTop
+const scrollToTop = document.querySelector(".scrollToTop");
+window.addEventListener("scroll", function () {
+  scrollToTop.classList.toggle("active", this.window.scrollY > 500);
+});
+
+scrollToTop.addEventListener("click", function () {
+  document.scrollToTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+//Responsive navigation menu togle
+
+const navBtn = document.querySelector(".nav-menu-btn");
+const navBar = document.querySelector(".nav");
+const navMenu = document.querySelector(".nav-menu");
+const navLinks = document.querySelectorAll(".nav-link");
+
+navBtn.addEventListener("click", function () {
+  navBtn.classList.toggle("close");
+  navBar.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+navLinks.forEach(function (link) {
+  link.addEventListener("click", function () {
+    navBtn.classList.remove("close");
+    navBar.classList.remove("active");
+    navMenu.classList.remove("active");
+  });
+});
+
+//Service section - Modal
 const serviceModal = document.querySelectorAll(".service-modal");
 const learnMoreBtn = document.querySelectorAll(".learn-more-btn");
 const modalCloseBtn = document.querySelectorAll(".modal-close-btn");
@@ -47,7 +107,7 @@ portfolioCloseBtn.forEach((button) => {
 
 // Swiper initialization
 
-var swiper = new Swiper(".client-swiper", {
+let swiper = new Swiper(".client-swiper", {
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
@@ -59,4 +119,22 @@ var swiper = new Swiper(".client-swiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+
+ScrollReveal({
+  reset: true,
+  distance: "60px",
+  duration: 2500,
+  delay: 100,
+});
+ScrollReveal().reveal(".home-info h1", { delay: 500, origin: "left" });
+ScrollReveal().reveal(".home-img", { delay: 600, origin: "right" });
+ScrollReveal().reveal(".media-icons a", {
+  delay: 700,
+  origin: "bottom",
+  interval: 200,
+});
+ScrollReveal().reveal(".home-info h3, .home-info p , .inner-info-link", {
+  delay: 600,
+  origin: "left",
 });
