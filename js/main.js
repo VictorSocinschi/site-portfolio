9a7aafa8083e1065714c21aa3f4679a628ad63e6
Victor Socinschi <chaosdaddy84@gmail.com>
2024-12-17 18:12:08 +0200

    Added  switch for dark theme

--- js/main.js
+++ js/main.js
@@ -58,6 +58,31 @@ navLinks.forEach(function (link) {
     navMenu.classList.remove("active");
   });
 });
+//Theme button Light/Dark
+const themeBtn = document.querySelector(".theme-btn");
+
+const getCurrentTheme = () =>
+  document.body.classList.contains("dark-theme") ? "dark" : "light";
+//Function to fing icon
+const getCurrentIcon = () =>
+  themeBtn.classList.contains("sun") ? "sun" : "moon";
+//Function switch theme
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  themeBtn.classList.toggle("sun");
+
+  localStorage.setItem("saved-theme", getCurrentTheme());
+  localStorage.setItem("saved-icon", getCurrentIcon());
+});
+const savedTheme = localStorage.getItem("saved-theme");
+const savedIcon = localStorage.getIcon("saved-icon");
+
+if (savedTheme) {
+  document.body.classList[savedTheme === "dark" ? "add" : "remove"](
+    "dark-theme"
+  );
+  themeBtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun");
+}
 
 //Service section - Modal
 const serviceModal = document.querySelectorAll(".service-modal");
